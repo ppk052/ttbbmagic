ca92803fad3743785a287efa17d1076e9420fee0
TATOGG <gimgibum21@gmail.com>
2024-10-20 00:19:11 +0900

    rasp 바뀐 코드들

--- javascript/script.js
+++ javascript/script.js
@@ -51,44 +51,37 @@ const circle = document.querySelector("div");
 // 웹소켓 연결
 const webSocket = new WebSocket("ws://localhost:8000");
 
-webSocket.onopen = function() {
-    console.log("Web Socket Connected");
-    webSocket.send('안녕하세요'); // 서버에 메시지 보내보기
+webSocket.onopen = function () {
+  console.log("Web Socket Connected");
+  webSocket.send("안녕하세요"); // 서버에 메시지 보내보기
 };
 
-webSocket.onmessage = function(message) {
-    console.log(message.data); // 콘솔 표시
-    if (pattern.test(message.data)) {
-        console.log("ok");
-        const info = pattern.exec(message.data);
-        console.log(typeof(info[0]));
-        console.log(info[1]);
-        if (info[1] == "0") {
-            circle.id = "";
-            write = false;
-            console.log("안보임");
-        } else {
-            circle.id = "visality";
-            write = true;
-            console.log("보임");
-        }
-        console.log(info[2] + "px");
-        circle.style.top = info[2] + "px";
-        console.log(info[3] + "px");
-        circle.style.left = info[3] + "px";
+webSocket.onmessage = function (message) {
+  console.log(message.data); // 콘솔 표시
+  if (pattern.test(message.data)) {
+    console.log("ok");
+    const info = pattern.exec(message.data);
+    console.log(typeof info[0]);
+    console.log(info[1]);
+    if (info[1] == "0") {
+      circle.id = "";
+      write = false;
+      console.log("안보임");
+    } else {
+      circle.id = "visality";
+      write = true;
+      console.log("보임");
     }
+    console.log(info[2] + "%");
+    circle.style.top = info[2] + "%";
+    console.log(info[3] + "%");
+    circle.style.left = info[3] + "%";
+  }
 };
 
 const button = document.querySelector("button");
 button.addEventListener("click", clicked);
 
 function clicked() {
-    document.exitFullscreen();
+  document.exitFullscreen();
 }
-
-
-
-
-
-
-
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-"""import websockets
+import websockets
 import asyncio
 import time
 
@@ -32,7 +32,7 @@ class server:
     #async def go(self):
         
 
-server1 = server([0,0,0],False)
+server1 = server([1,0,0],False)
 #server1.go()
 print("go ok")
 while not server.connected:
@@ -41,129 +41,4 @@ while not server.connected:
 for i in range(5):
     server1.send([1,i*100,250])
     time.sleep(1)
-    print(i)"""
-
-
-"""import asyncio
-import websockets
-
-connected_clients = set()
-
-async def handler(websocket, path):
-    connected_clients.add(websocket)
-    try:
-        async for message in websocket:
-            print(f"Received message: {message}")
-            # Echo the received message back to the client
-            await websocket.send(message)
-    except websockets.ConnectionClosed:
-        print("Connection closed")
-    finally:
-        connected_clients.remove(websocket)
-
-async def main():
-    async with websockets.serve(handler, "localhost", 8000):
-        await asyncio.Future()  # run forever
-
-# Run the server
-asyncio.run(main())"""
-
-
-"""import asyncio
-import websockets
-
-connected_clients = set()
-
-async def handler(websocket, path):
-    connected_clients.add(websocket)
-    try:
-        async for message in websocket:
-            print(f"Received message: {message}")
-            # Echo the received message back to the client
-            await websocket.send(message)
-    except websockets.ConnectionClosed:
-        print("Connection closed")
-    finally:
-        connected_clients.remove(websocket)
-
-async def send_message_to_clients():
-    while True:
-        message = input("Enter message to send to all clients: ")
-        if connected_clients:  # Check if there are connected clients
-            await asyncio.gather(*(client.send(message) for client in connected_clients))
-
-async def main():
-    server = await websockets.serve(handler, "localhost", 8000)
-    await asyncio.gather(server.wait_closed(), send_message_to_clients())
-
-# Run the server
-asyncio.run(main())"""
-
-"""import asyncio
-import websockets
-
-connected_clients = set()
-
-async def handler(websocket, path):
-    # 클라이언트 연결 시 메시지 출력
-    print("Client connected")
-    connected_clients.add(websocket)
-    try:
-        async for message in websocket:
-            print(f"Received message: {message}")
-            # Echo the received message back to the client
-            await websocket.send(message)
-    except websockets.ConnectionClosed:
-        print("Connection closed")
-    finally:
-        connected_clients.remove(websocket)
-        # 클라이언트 연결 종료 시 메시지 출력
-        print("Client disconnected")
-
-async def send_message_to_clients():
-    while True:
-        message = await asyncio.get_event_loop().run_in_executor(None, input, "Enter message to send to all clients: ")
-        if connected_clients:  # Check if there are connected clients
-            await asyncio.gather(*(client.send(message) for client in connected_clients))
-
-async def main():
-    server = await websockets.serve(handler, "localhost", 8000)
-    print("WebSocket server started on ws://localhost:8000")
-    await asyncio.gather(server.wait_closed(), send_message_to_clients())
-
-# Run the server
-asyncio.run(main())"""
-import asyncio
-import websockets
-
-class WebSocketServer:
-    def __init__(self, host='localhost', port=8000):
-        self.host = host
-        self.port = port
-        self.connected_clients = set()
-
-    async def handler(self, websocket, path):
-        print("Client connected")
-        self.connected_clients.add(websocket)
-        try:
-            async for message in websocket:
-                print(f"Received message: {message}")
-                await websocket.send(message)
-        except websockets.ConnectionClosed:
-            print("Connection closed")
-        finally:
-            self.connected_clients.remove(websocket)
-            print("Client disconnected")
-
-    async def send_message_to_clients(self):
-        while True:
-            message = await asyncio.get_event_loop().run_in_executor(None, input, "Enter message to send to all clients: ")
-            if self.connected_clients:
-                await asyncio.gather(*(client.send(message) for client in self.connected_clients))
-
-    async def start(self):
-        server = await websockets.serve(self.handler, self.host, self.port)
-        print(f"WebSocket server started on ws://{self.host}:{self.port}")
-        await asyncio.gather(server.wait_closed(), self.send_message_to_clients())
-
-
+    print(i)
