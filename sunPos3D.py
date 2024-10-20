48f370b02fcd65825317adc6d8cf5b174d6d44fa
TATOGG <gimgibum21@gmail.com>
2024-10-20 13:43:59 +0900

    Pos3D 모듈회

--- sunPos3D.py
+++ sunPos3D.py
@@ -1,14 +1,13 @@
 import math
 
-alpha, beta = 1, 500 # 나중에 바꿀곳
+def runSunPos3D(alpha, beta):
+    a = 4608 / 2
+    b = 2592 / 2
 
-a = 4608 / 2
-b = 2592 / 2
+    theta0 = math.pi / 180 * 51
+    pi0 = math.pi / 180 * (67/2)
 
-theta0 = math.pi / 180 * 51
-pi0 = math.pi / 180 * (67/2)
+    theta1 = math.atan(alpha * math.tan(theta0) / a)
+    pi1 = math.atan(beta * math.tan(pi0) / b)
 
-theta1 = math.atan(alpha * math.tan(theta0) / a)
-pi1 = math.atan(beta * math.tan(pi0) / b)
-
-print("<", -a/math.tan(theta0),",", a*math.tan(theta1)/math.tan(theta0), ",", b*math.tan(pi1)/math.tan(pi0), ">")
+    print("<", -a/math.tan(theta0),",", a*math.tan(theta1)/math.tan(theta0), ",", b*math.tan(pi1)/math.tan(pi0), ">")
