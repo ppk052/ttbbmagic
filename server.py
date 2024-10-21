6a2b35d9289a039177b2892f147e11bd4cbe9e2c
정승민 <ppk052@naver.com>
2024-10-21 18:07:38 +0900

    크롬열기 수정

--- server.py
+++ server.py
@@ -25,11 +25,11 @@ class server:
         self.message = message  
         self.status = False  
         start_server = websockets.serve(self.hello, "localhost", 8000)
+        os.system("chromium-browser --kiosk /home/pi/ttbbmagic/index.html")
         asyncio.get_event_loop().run_until_complete(start_server)
         asyncio.get_event_loop().run_forever() 
 
     async def hello(self,websocket, path):
-        os.system("chromium-browser --kiosk /home/pi/ttbbmagic/index.html")
         name = await websocket.recv()
         print("connected")
         server.connected = True
