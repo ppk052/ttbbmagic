83a2b4c127b0fd5b9949c719ba248265b0109565
TATOGG <gimgibum21@gmail.com>
2024-10-23 04:11:56 +0900

    tt

--- sunPos3D.py
+++ sunPos3D.py
@@ -4,8 +4,8 @@ def runSunPos3D(alpha, beta):
     a = 640 / 2
     b = 480 / 2
 
-    theta0 = math.pi / 180 * 51
-    pi0 = math.pi / 180 * (67/2)
+    theta0 = math.pi / 180 * 27
+    pi0 = math.pi / 180 * (41/2)
 
     theta1 = math.atan(alpha * math.tan(theta0) / a)
     pi1 = math.atan(beta * math.tan(pi0) / b)
