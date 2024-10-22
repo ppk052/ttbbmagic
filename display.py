994ff1c48daf5691d9e73ba50107129c18a9c599
정승민 <ppk052@naver.com>
2024-10-22 17:05:06 +0900

    디스플레이 좌표계산모듈추가

--- display.py
+++ display.py
@@ -3,6 +3,6 @@ import math
 #eyeleft,eyeright,sun은 길이3의 배열
 def caldisplay(eyeleft,eyeright,sun):
     result = [0,0]
-    #여기에 디스플레이 2차원 좌표계산 알고리즘넣기
+    #여기에 디스플레이 2차원 좌표계산 해서 result에 넣기
     
     return result
