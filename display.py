f4e7c25d2e1d47f094391e8db22b41c14d98fdeb
정승민 <ppk052@naver.com>
2024-10-22 17:42:43 +0900

    디스플레이 묘듈식 변수이름수정

--- display.py
+++ display.py
@@ -8,7 +8,7 @@ def caldisplay(eyeleft,eyeright,sun):
     for i in range(3):
         eyeforcal[i]=(eyeleft[i]+eyeright[i])/2
     #디스플레이 3차원좌표
-    temp = [0,0,0]
+    display3D= [0,0,0]
     #최종 디스플레이 퍼센트 = result, max_x는 디스플레이 계산식의 A, max_y는 디스플레이 계산식의 B 나머지는 동일
     result = [0,0]
     max_x = 0
@@ -20,7 +20,7 @@ def caldisplay(eyeleft,eyeright,sun):
     b = eyeforcal[1]
     c = eyeforcal[2]
     # 디스플레이 3차원좌표 계산식
-    temp = [(c*e-a*g)*math.cos/(e*math.sin-g*math.cos),((c*p-b*g)*math.cos+(b*e-a*f)*math.sin)/(e*math.sin-g*math.cos),(c*e-a*g)*math.sin/(e*math.sin-g*math.cos)] 
+    display3D = [(c*e-a*g)*math.cos/(e*math.sin-g*math.cos),((c*p-b*g)*math.cos+(b*e-a*f)*math.sin)/(e*math.sin-g*math.cos),(c*e-a*g)*math.sin/(e*math.sin-g*math.cos)] 
     # 디스플레이 퍼센트 계산식
-    result = [(temp[1]+max_x/2)/max_x*100,(max_y-temp[2]/math.sin)/max_y*100]
+    result = [(display3D[1]+max_x/2)/max_x*100,(max_y-display3D[2]/math.sin)/max_y*100]
     return result
