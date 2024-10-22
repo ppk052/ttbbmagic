dc64c5d19ace545094b126428c01a9f9b36ae267
TATOGG <gimgibum21@gmail.com>
2024-10-23 00:31:43 +0900

    changexy

--- eyePos3D.py
+++ eyePos3D.py
@@ -1,8 +1,13 @@
 import math
 
+def changeX(k):
+    return k * 3.4 * math.tan(25.67*math.pi/180) / 320
+def changeY(k):
+    return k * 3.4 * math.tan(19.8254*math.pi/180) / 320
+
 def runEyePos3D(cam0x,cam0y,cam1x,cam1y):
     
-    b, a, e, d = cam0x,cam0y,cam1x,cam1y #나중에 바꿀곳
+    b, a, e, d = changeX(cam0x),changeY(cam0y),changeX(cam1x),cam1y #나중에 바꿀곳
     l = 3.4 #카메라 초점거리
 
     if e==0:
