05c570984dcd53ee4a9f52638ebee45f5dc4ee83
TATOGG <gimgibum21@gmail.com>
2024-10-22 22:04:12 +0900

    eyePos3D 예외처리

--- eyePos3D.py
+++ eyePos3D.py
@@ -5,6 +5,11 @@ def runEyePos3D(cam0x,cam0y,cam1x,cam1y):
     b, a, e, d = cam0x,cam0y,cam1x,cam1y #나중에 바꿀곳
     l = 0.2 #카메라 초점거리
 
+    if e==0:
+        e=1
+    if b==0:
+        b=1
+
     theta = b/l
     pi = e/l
     #theta = math.pi / 4
